99b3b358550eb0c9aa6b75e02f0f73591aed088b
Pondelok <100646287+Pondelok@users.noreply.github.com>
2022-03-29 11:35:38 +0200

    Update thingProperties.h

--- thingProperties.h
+++ thingProperties.h
@@ -2,8 +2,8 @@
 #include <Arduino_ConnectionHandler.h>
 
 
-const char THING_ID[]           = "294978c8-d2f8-4106-a8c6-4cc49ce8e94a";  //Enter THING ID
-const char DEVICE_LOGIN_NAME[]  = "c9b62748-7db9-494f-8b54-e7bd930c5b0e"; //Enter DEVICE ID
+const char THING_ID[]           = "***";  //Enter THING ID
+const char DEVICE_LOGIN_NAME[]  = "***"; //Enter DEVICE ID
 
 const char SSID[]               = SECRET_SSID;    // Network SSID (name)
 const char PASS[]               = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
